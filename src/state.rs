f9920eb48ca18df8c7155ee45103c2c4970db160
franz <mattefranzz@gmail.com>
2024-04-19 17:34:10 +0200

    state to_string implemented

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,5 @@
+use crate::compiler::utils::get_register_alias;
+
 
 pub struct State{
     pub registers: [u32; 32],
@@ -18,5 +20,35 @@ impl State {
             ir: 0,
             pc: 0
         };
+    }
+
+    fn var_to_string(label1: &str, value1: &u32, label2: &str, value2: &u32) -> String{
+        return format!("{}\n| {:2} | {:10} | {:2} | {:10} |", Self::end_line(), label1, value1, label2, value2);
+    }
+
+    fn end_line() -> String {
+        let mut l: [char; 37] = ['-'; 37];
+        l[0] = '+'; l[36] = '+';
+        return l.iter().collect::<String>();
+    }
+    
+
+    pub fn to_string(&self) -> String{
+
+        let registers_table = (0..16)
+            .map(|i| Self::var_to_string(
+                get_register_alias(&i.to_string()), 
+                &self.registers[i], 
+                get_register_alias(&(i+16).to_string()), 
+                &self.registers[i+16]) + "\n")
+            .collect::<String>();
+
+        return String::from(
+            Self::var_to_string("PC", &self.pc, "IR", &self.ir) + "\n" +
+            &Self::var_to_string("LO", &self.lo, "HI", &self.hi) + "\n" + 
+            &Self::end_line() + "\n\n" +
+            &registers_table +
+            &Self::end_line()
+        );
     }
 }
