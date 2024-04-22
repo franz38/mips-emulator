cdb9cbb0047bf81fe3ca322d3421a71e9f21b6af
franz <mattefranzz@gmail.com>
2024-04-23 00:39:00 +0200

    step parameter test setup

---
+++ tests/js_wrapper.rs
@@ -0,0 +1,19 @@
+use mips_emulator::compile_and_execute_js;
+
+
+#[test]
+fn addi() {
+    let mut v_state: Vec<u32> = compile_and_execute_js(String::from("ADDI $1 $0 22"), None);
+    assert_eq!(v_state[4], 0);
+    assert_eq!(v_state[5], 22);
+    
+    v_state = compile_and_execute_js(String::from("ADDI $1 $0 22\nADDI $2 $0 33"), None);
+    assert_eq!(v_state[4], 0);
+    assert_eq!(v_state[5], 22);
+    assert_eq!(v_state[6], 33);
+
+    // v_state =
+    //     compile_and_execute_js(String::from("ADDI $1 $0 22"), Some(State::blank_state()));
+    // assert_eq!(v_state[4], 0);
+    // assert_eq!(v_state[5], 22);
+}
