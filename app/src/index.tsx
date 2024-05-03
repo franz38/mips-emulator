51a7cdfc33193a7d7ee54cf43b6b242e08dc8b9f
franz <mattefranzz@gmail.com>
2024-05-03 15:59:50 +0200

    scss implemented - fix for safari

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,7 +2,7 @@
 import { render } from 'solid-js/web';
 import 'solid-devtools';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 
 const root = document.getElementById('root');
